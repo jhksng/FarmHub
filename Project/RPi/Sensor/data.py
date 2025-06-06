adf522c68d8974c7f52109d65fc2fa2abb30dacc
jhksng <jeonghokim2727@gmail.com>
2025-06-07 04:13:53 +0900

    Update data.py

--- Project/RPi/Sensor/data.py
+++ Project/RPi/Sensor/data.py
@@ -15,24 +15,25 @@ db = mysql.connector.connect(
 
 cursor = db.cursor()
 
-while True:
-    if arduino.in_waiting > 0:
-        try:
-            data = arduino.readline().decode('utf-8').strip()
-            values = data.split(",")
-            soil_value = float(values[0].strip())  
-            water_value = float(values[1].strip())  
-            timestamp = datetime.now()
-            temp = 10  
-            humi = 20  
-            query = "INSERT INTO sensor_data (soil1, water, temp, humi, timestamp) VALUES (%s, %s, %s, %s, %s)"
-            cursor.execute(query, (soil_value, water_value, temp, humi, timestamp))
-            db.commit()
+try:
+    while True:
+        if arduino.in_waiting > 0:
+            try:
+                data = arduino.readline().decode('utf-8').strip()
+                values = data.split(",")
+                soil_value = float(values[0].strip())  
+                water_value = float(values[1].strip())  
+                timestamp = datetime.now()
+                temp = 10  
+                humi = 20  
+                query = "INSERT INTO sensor_data (soil1, water, temp, humi, timestamp) VALUES (%s, %s, %s, %s, %s)"
+                cursor.execute(query, (soil_value, water_value, temp, humi, timestamp))
+                db.commit()
 
-            print("Data inserted into DB.")
-        except Exception as e:
-            print(f"Error parsing data: {e}")
-    time.sleep(1)
+                print("Data inserted into DB.")
+            except Exception as e:
+                print(f"Error parsing data: {e}")
+        time.sleep(1)
 
 finally:
     cursor.close()

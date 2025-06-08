de75abb47f864372ebf4e6395202348b1e623f53
jhksng <jeonghokim2727@gmail.com>
2025-06-08 17:29:57 +0900

    Update test.ino

--- Project/Arduino/test.ino
+++ Project/Arduino/test.ino
@@ -6,21 +6,15 @@ void setup() {
 }
 
 void loop() {
-	float soil_val = 0;
-	float water_val = 0;
-
-	for(int i=0; i<6; i++){
-		int soil_R = analogRead(soil);
-		int water_R = analogRead(water);
+	int soil_R = analogRead(soil);
+	int water_R = analogRead(water);
 	
-		soil_val += (1023 - soil_R) * 100.0 / 1023;
-		water_val += water_R * 100.0 / 1023;
-		delay(10000);
-	}
-	soil_val /= 6;
-	water_val /= 6;
+	float soil_val += (1023 - soil_R) * 100.0 / 1023;
+	float water_val += water_R * 100.0 / 1023;
 
 	Serial.print(soil_val);
 	Serial.print(",");
 	Serial.println(water_val);
+
+	delay(10000);
 }

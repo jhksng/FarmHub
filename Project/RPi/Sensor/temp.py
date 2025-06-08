f0aa69b20db8f114727e252a98c60e8dfdd2301f
jhksng <jeonghokim2727@gmail.com>
2025-06-08 18:08:54 +0900

    Delete Project/RPi/Sensor/temp.py

--- Project/RPi/Sensor/temp.py
+++
@@ -1,9 +0,0 @@
-import board
-import busio
-import adafruit_sht31d
-
-i2c = board.I2C()
-sensor = adafruit_sht31d.SHT31D(i2c ,address=0x44)
-print("sensor obj", sensor)
-print("temp:", sensor.temperature)
-print("humi:", sensor.relative_humidity)

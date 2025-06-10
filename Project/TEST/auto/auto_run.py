c36f72c9339472373f3825ecd464ee6809f1b3c0
jhksng <jeonghokim2727@gmail.com>
2025-06-11 07:29:46 +0900

    Delete Project/TEST directory

--- Project/TEST/auto/auto_run.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-subprocess.Popen(["python3", "/home/pi/web/print.py"])
-subprocess.Popen(["python3", "/home/pi/web/control.py"])
-subprocess.Popen(["python3", "/home/pi/web/camera.py"])
